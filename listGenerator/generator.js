c424121f300f63ec7bc382ee5445bba0e1e309e5
Soxori <jakub.panek@educanet.cz>
2022-11-22 20:23:42 +0100

    asiLepsi

--- listGenerator/generator.js
+++ listGenerator/generator.js
@@ -3,7 +3,7 @@ let surnames = ["Novák", "Novotný", "Svoboda", "Svobodová", "Nováková", "Dv
 
 // input data for generator
 const dtoIn = {
-    count: 50,
+    count: 5,
     age: {
       min: 19,
       max: 35
@@ -19,12 +19,46 @@ function main(dataInput) {
             birthdate: randomBirthdate(),
             name: randomName(),
             surname: randomSurname(),
-            workload: randomWorkload()
+            workload: randomWorkload(),
         });
     }
+    console.log("Check");
+    console.log(summary(dtoOut));
     return dtoOut;
 }
 console.log(main(dtoIn));
+
+// function for sorting employees by workload
+function summary(listOfEmployees) {
+  // get amount of employees in dToOut array
+  let amountOfEmployees = listOfEmployees.length;
+  // get amount of employees with workload 10, 20, 30, 40 in one cycle
+  let workload10 = 0;
+  let workload20 = 0;
+  let workload30 = 0;
+  let workload40 = 0;
+  for (let i = 0; i < amountOfEmployees; i++) {
+    if (listOfEmployees[i].workload === 10) {
+      workload10++;
+    } else if (listOfEmployees[i].workload === 20) {
+      workload20++;
+    } else if (listOfEmployees[i].workload === 30) {
+      workload30++;
+    } else if (listOfEmployees[i].workload === 40) {
+      workload40++;
+    }
+  }
+  
+  const summaryArray = [];
+  summaryArray.push({
+    total: amountOfEmployees,
+    workload10: workload10,
+    workload20: workload20,
+    workload30: workload30,
+    workload40: workload40,
+  });
+  return summaryArray;
+}
 
 // functions for generating random names, surnames, ages, workloads and gender of employees
 function randomName() {
@@ -35,7 +69,7 @@ function randomSurname() {
 }
 function randomBirthdate() {
   let age = Math.floor(Math.random() * (dtoIn.age.max - dtoIn.age.min + 1)) + dtoIn.age.min;
-  return new Date(new Date().setFullYear(new Date().getFullYear() - age)).toISOString();
+  return new Date(new Date().setFullYear(new Date().getFullYear() - age, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28))).toISOString();
 }
 function randomWorkload() {
   return Math.round(Math.random() * 3 + 1) * 10;
